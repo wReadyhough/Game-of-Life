62021342d6e6feaf98532fd1190c6b11481c0205
willReadyhough <66326631+wReadyhough@users.noreply.github.com>
2021-06-16 13:19:20 +0000

    More time stuff

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono;
 int main() {
   auto total = 0;
   int numIterations = 100;
-  auto start = high_resolution_clock::now();
+  
   srand(time(NULL));
   int numAlive, row, column;
   cout<<"Enter number of rows/columns: ";
@@ -17,17 +17,17 @@ int main() {
   column = row;
   cout<<"Enter number of alive cells("<<row*row<<" total spaces): ";
   cin>>numAlive;
+  auto start = high_resolution_clock::now();
   for(int i = 0; i < numIterations; i++){
     boardClass myBoard(row,column);
-    myBoard.printBoard();
-    /*cout<<"How many cells do you want alive: ";
-    cin>>numAlive;*/
+    //myBoard.printBoard();
     myBoard.addAliveCells(numAlive);
-    cout<<"Board after randomly inserting "<<numAlive<<" alive cells: "<<endl;
+   /* cout<<"Board after randomly inserting "<<numAlive<<" alive cells: "<<endl;
     myBoard.printBoard();
-    cout<<"Board after updating: "<<endl;
+    cout<<"Board after updating: "<<endl;*/
     myBoard.update();
-    myBoard.printBoard();
+    myBoard.update();
+    //myBoard.printBoard();
   }
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<microseconds>(stop - start);
