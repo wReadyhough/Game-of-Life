eae861b9431b2ea699a2b05b391e5b8cbbdf098b
willReadyhough <66326631+wReadyhough@users.noreply.github.com>
2021-06-16 12:52:03 +0000

    More stuff. Not important

--- main.cpp
+++ main.cpp
@@ -10,19 +10,22 @@ int main() {
   srand(time(NULL));
   auto start = high_resolution_clock::now();
   int numAlive, row, column;
-  row = 5;
-  column = 6;
-  int numIterations = 1000;
+  row = 10;
+  column = 10;
+  int numIterations = 100;
   for(int i = 0; i < numIterations; i++){
     boardClass myBoard(row,column);
     myBoard.printBoard();
     /*cout<<"How many cells do you want alive: ";
     cin>>numAlive;*/
-    numAlive = 10;
+    numAlive = 50;
     myBoard.addAliveCells(numAlive);
     cout<<"Board after randomly inserting "<<numAlive<<" alive cells: "<<endl;
     myBoard.printBoard();
     cout<<"Board after updating: "<<endl;
+    myBoard.update();
+    myBoard.printBoard();
+    cout<<"Board after updating again: "<<endl;
     myBoard.update();
     myBoard.printBoard();
   }
